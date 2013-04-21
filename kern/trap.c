f17bb0faa30e2bc77b5489b1592dd76db31be0c4
misterx <xmorozov@gmail.com>
2013-04-21 20:18:20 +0400

    fev

--- kern/trap.c
+++ kern/trap.c
@@ -448,10 +448,15 @@ page_fault_handler(struct Trapframe *tf)
 
 	// LAB 4: Your code here.
 
+	unsigned int orig_esp;
+	struct UTrapframe utf;
+
 	// Destroy the environment that caused the fault.
-	cprintf("[%08x] user fault va %08x ip %08x\n",
-		curenv->env_id, fault_va, tf->tf_eip);
-	print_trapframe(tf);
-	env_destroy(curenv);
+	if(!curenv->env_pgfault_upcall) {
+	    cprintf("no page fault handler installed.\n");
+	    cprintf("[%08x] user fault va %08x ip %08x\n", curenv->env_id, fault_va, tf->tf_eip);
+	    print_trapframe(tf);
+	    env_destroy(curenv);
+    }
 }
 
