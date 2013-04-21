d54fecd3a5dd618d97dafde78f88ee6501b11dd7
misterx <xmorozov@gmail.com>
2013-04-21 13:49:38 +0400

    erg

--- lib/ipc.c
+++ lib/ipc.c
@@ -22,9 +22,33 @@
 int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
+    struct Env *env = 0;
+    int ret = 0;
+
+	if(!pg) {
+	    pg = (void *)USTACKTOP;
+	}
+
+	if((ret = sys_ipc_recv(pg)) < 0) {
+	    if(from_env_store) {
+	        *from_env_store = 0;    
+	    }
+	    if(perm_store) {
+	        *perm_store = 0;
+	    }
+	    return ret;
+	} else {
+	    if(from_env_store) {
+	        *from_env_store = thisenv->env_ipc_from;
+	    }
+	    if(perm_store) {
+	        *perm_store = thisenv->env_ipc_perm;
+	    }
+	    return thisenv->env_ipc_value;
+	}
+	
 	// LAB 4: Your code here.
 	panic("ipc_recv not implemented");
-	return 0;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
@@ -36,8 +60,21 @@ ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 //   If 'pg' is null, pass sys_ipc_recv a value that it will understand
 //   as meaning "no page".  (Zero is not the right value.)
 void
-ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
+ipc_send(envid_t to_env, uint32_t value, void *pg, int perm)
 {
+	int ret;
+	
+	//wait for ipc_recv
+	while(1) {
+	    if((ret = sys_ipc_try_send(to_env, value, pg, perm)) >= 0) {
+	        return;
+	    }
+    	if(ret != -E_IPC_NOT_RECV) {
+	        panic("sys_ipc_try_send problems");	    
+   	    }
+   	    sys_yield();
+   	}
+	
 	// LAB 4: Your code here.
 	panic("ipc_send not implemented");
 }
