a7eaeb4fca9c6e012d7148953635804f3a1bf4a3
misterx <xmorozov@gmail.com>
2013-04-30 02:52:02 +0400

    erg

--- inc/lib.h
+++ inc/lib.h
@@ -41,6 +41,7 @@ char*	readline(const char *buf);
 // syscall.c
 void	sys_cputs(const char *string, size_t len);
 int	sys_cgetc(void);
+int	sys_set_priority(int);
 envid_t	sys_getenvid(void);
 int	sys_env_destroy(envid_t);
 void	sys_yield(void);
