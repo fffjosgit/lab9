7508946716ddae1f301d20eae61da116eb3b28ec
misterx <xmorozov@gmail.com>
2013-05-27 23:26:44 +0400

    trg

--- kern/sched.c
+++ kern/sched.c
@@ -31,26 +31,26 @@ sched_yield(void)
 	uint32_t first_eid = (++envid) % NENV;
 	int next_envid;
 	int i;
-	struct Env env;
+	struct Env *env;
 	int min_cc;
 
 	//perform_io_simulation();
 
 	for(i = 0; i < NENV; i++) {
-	    if((env = envs[i]).env_priority == ENV_PRIORITY_HIGH) {
+	    if((env = &envs[i])->env_priority == ENV_PRIORITY_HIGH) {
 	        
-	        cprintf("[%08x]: %d %d %d %d. \n", env.env_id, 
-	            env.env_c, env.env_p, env.env_cp, env.env_cc);
+	        cprintf("[%08x]: %d %d %d %d. \n", env->env_id, 
+	            env->env_c, env->env_p, env->env_cp, env->env_cc);
 
-	        if(env.env_cp <= 0) {
-	            env.env_cp = env.env_p;
-	            if(env.env_cc > 0) {
+	        if(env->env_cp <= 0) {
+	            env->env_cp = env->env_p;
+	            if(env->env_cc > 0) {
 	                panic("Sched: i'm a real!");	                
 	            } else {
-	                env.env_cc = env.env_c;    
+	                env->env_cc = env->env_c;    
 	            }
 	        }
-	        envs[i].env_cp--;	        	            
+	        envs->env_cp--;	        	            
 	    }
 	}
 	
@@ -62,9 +62,9 @@ sched_yield(void)
 	}
 
 	for(i = 0; i < NENV; i++) {
-	    if(((env = envs[i]).env_priority == ENV_PRIORITY_HIGH) 
-	    && ((env.env_status == ENV_RUNNABLE) || (env.env_status == ENV_RUNNING)) ) {
-	        if((env.env_cc > 0) && (env.env_cc <= min_cc)) {
+	    if(((env = &envs[i])->env_priority == ENV_PRIORITY_HIGH) 
+	    && ((env->env_status == ENV_RUNNABLE) || (env->env_status == ENV_RUNNING)) ) {
+	        if((env->env_cc > 0) && (env->env_cc <= min_cc)) {
 	            next_envid = i;
 	        }                                               
 	    }    
