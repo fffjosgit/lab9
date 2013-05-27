8982778566b33e01416e99b2ee2c86f096343b2a
misterx <xmorozov@gmail.com>
2013-05-27 23:22:43 +0400

    sdf

--- user/sched_test.c
+++ user/sched_test.c
@@ -7,7 +7,7 @@ umain(int argc, char **argv)
 {
 	int i;
 
-	sys_make_me_real(100, 100, 100);
+	sys_make_me_real(10, 10, 10);
 
 	cprintf("Hello, I am real-time environment %08x.\n", thisenv->env_id);
 	
